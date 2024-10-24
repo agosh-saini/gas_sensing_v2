6b7ce16c9e76c2ad1f3e30efb731c1293337b2ee
Agosh Saini <33809549+agosh-saini@users.noreply.github.com>
2024-10-24 10:36:45 -0400

    fixed reference

--- Sensing_Automation.bat
+++ Sensing_Automation.bat
@@ -4,4 +4,4 @@ if not exist logs (
 )
 set timestamp=%date:~-4,4%-%date:~-10,2%-%date:~-7,2%_%time:~0,2%-%time:~3,2%-%time:~6,2%
 set timestamp=%timestamp: =0%
-python your_script.py > logs\log_%timestamp%.txt 2>&1
+python main.py > logs\log_%timestamp%.txt 2>&1
